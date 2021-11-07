6946cfc09729433d4f007d1e84ba77b0063396b0
Katarzyna Kawala <kat.kawala@gmail.com>
2021-11-07 16:45:34 +0100

    rest methods using gorilla

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/katarzynakawala/Products/handlers"
 )
 
@@ -20,8 +21,19 @@ func main() {
 	ph := handlers.NewProducts(l)
 	
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
+
+	//sm.Handle("/products", ph)
 	
 
 	s := &http.Server{
